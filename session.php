c652c4adae8f8bcdc5b640cc35e8610124a17553
sergio0811 <114895717+sergio0811@users.noreply.github.com>
2022-10-03 17:57:54 -0300

    Delete session.php

--- session.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	include('conn.php');
-
-	$query=$conn->query("select * from user where userid='".$_SESSION['user']."'");
-	$row=$query->fetch_array();
-
-	$user=$row['username'];
-?>
